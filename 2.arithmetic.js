e866fd43955b3f5b7d89b8424daa474c301e8e63
Jay <bjw1622@gmail.com>
2022-04-06 11:47:10 +0900

    add 2.arithmetice.js

---
+++ 2.arithmetic.js
@@ -0,0 +1,12 @@
+//산술 연산자 (Arithmetic operators)
+// % 나머지 값
+// ** 거듭제곱
+
+console.log(5 % 2);
+console.log(5 ** 2);
+
+// + 연산자 주의점!
+let text = '1'
+text = '1' + 1
+console.log(text); // 숫자와 문자열을 더하면 문자열로 변환됨
+console.log(typeof(text));
