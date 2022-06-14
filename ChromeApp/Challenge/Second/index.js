4000eed9cfbb6b40ee452d23ceee390024319d4a
Jay <bjw1622@gmail.com>
2022-06-14 22:24:15 +0900

    add play button script

---
+++ ChromeApp/Challenge/Second/index.js
@@ -0,0 +1,17 @@
+const $ = (element)=>{
+    return document.querySelector(element)};
+
+// Generate 값 가져오기
+const generate = $("#generate__number").addEventListener("click", ()=>{
+    return $("#generate__number").value;
+});
+
+// Guess 값 가져오기
+const guess = $("#guess__number").addEventListener("click", ()=>{
+    return $("#guess__number").value;
+});
+
+// Play button 클릭
+$("#guess__number").addEventListener("click", ()=>{
+    return $("#guess__number").value;
+});
